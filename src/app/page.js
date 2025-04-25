4bb50ffab255efb7524c00ae96f057546c0d8a79
Mostafa Nouri <nouri.mst06@outlook.com>
2025-04-25 17:08:36 +1000

    change

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 'use client'
 
-import TimeDisplay from '@/components/TimeDisplay';
+import EventsPage from "./(routes)/events/[id]/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       <main>
-        <TimeDisplay />
+        <EventsPage />
       </main>
     </div>
   );
